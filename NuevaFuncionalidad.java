98c671ef3399ae50386bd9daee1e4362c185eda4
Lucio <155954020+Luciocf2@users.noreply.github.com>
2024-03-08 11:15:34 +0100

    Update NuevaFuncionalidad.java

--- NuevaFuncionalidad.java
+++ NuevaFuncionalidad.java
@@ -5,6 +5,7 @@
  */
 public class NuevaFuncionalidad {
     public static void main(String[] args){
-        System.out.println(" Hola");
+        System.out.println(" Buenas tardes guapass");
+
     }
 }
